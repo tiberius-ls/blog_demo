d907aa5c931707f56319ce92af990af1c754a970
tiberius-ls <ogbonnaemeka543@gmail.com>
2023-08-05 01:07:20 +0100

    adding notification gem for notifications

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -9,10 +9,33 @@ export default class extends Controller {
     event.stopPropagation();
     const formID = event.params["form"];
     const commentBodyID = event.params["body"];
-    const form = document.getElementById(formID)
+    const editButtonID = event.params["edit"];
+
+
+    const form = document.getElementById(formID);
+    const commentBody = document.getElementById(commentBodyID);
+    const editButton = document.getElementById(editButtonID);
+
+
     form.classList.toggle("d-none");
     form.classList.toggle("mt-5");
-    const commentBody = document.getElementById(commentBodyID)
     commentBody.classList.toggle("d-none");
+    
+    this.toggleEditButton(editButton);
+  }
+
+  toggleEditButton(editButton) {
+    if (editButton.innerText == "Edit") {
+      editButton.innerText = "Cancel";
+      this.toggleEditButtonClass(editButton);
+    } else {
+      editButton.innerText = "Edit";
+      this.toggleEditButtonClass(editButton);
+    }
+  }
+
+  toggleEditButtonClass(editButton) {
+    editButton.classList.toggle("btn-secondary");
+    editButton.classList.toggle("btn-info");
   }
 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :notifications, as: :recipient, dependent: :destroy
 end
